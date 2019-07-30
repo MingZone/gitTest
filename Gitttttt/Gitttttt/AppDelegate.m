1dee61a6a5c25fb8aedccf847452809b3d22b27b
刘汝明 <305255147@qq.com>
2019-07-30 12:23:05 +0800

    二次修改

--- Gitttttt/Gitttttt/AppDelegate.m
+++ Gitttttt/Gitttttt/AppDelegate.m
@@ -18,6 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"master");
+    NSLog(@"二次修改");
     return YES;
 }
 
